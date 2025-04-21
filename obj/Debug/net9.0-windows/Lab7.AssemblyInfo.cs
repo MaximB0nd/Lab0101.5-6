21a159398821047ca026d186c299c7baa16d5923
MaximB0nd <sersim11@yandex.ru>
2025-04-21 09:48:20 +0300

    task 5.1, 5.2

--- obj/Debug/net9.0-windows/Lab7.AssemblyInfo.cs
+++ obj/Debug/net9.0-windows/Lab7.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab7")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a7566a80f0950b45aba5747e74cd4cf7a9e9d0c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4758aebe7cf196ed5986ec1e7f0f7363142c05d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab7")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab7")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
